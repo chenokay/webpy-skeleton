0b1e2b1e8440084033ad5b063fd1176a89e50617
chenokay <chenokay@gmail.com>
2015-01-02 19:09:30 +0800

    add env parameters recognization

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ import web
 import config
 import model as m
 import sys
+import http_driver 
+import json
+
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
@@ -53,17 +56,140 @@ t_globals['render'] = lambda t, *args: render._template(t)(*args)
 
 class Index:
     def __init__(self):
-        self.ip2region = "http://apistore.baidu.com/microservice/iplookup?ip="
-        self.region2code = "http://apistore.baidu.com/microservice/cityinfo?cityname="
-        self.code2weather = "http://apistore.baidu.com/microservice/weather?cityid="
-        self.region2aqi = "http://apistore.baidu.com/microservice/aqi?city="
+        self.ip2region = "/microservice/iplookup?ip="
+        self.region2code = "/microservice/cityinfo?cityname="
+        self.code2weather = "/microservice/weather?cityid="
+        self.region2aqi = "/microservice/aqi?city="
+
+        self.http_handle=http_driver.HttpDriver('apistore.baidu.com', config.logger)
+
+        self.ip = '127.0.0.1'
+        self.region = ''
+        self.regioncode = ''
+        self.weather = ''
+        self.centigrade = ''
+        self.wind = ''
+
+        self.aqi = ''
+        self.level = ''
 
     def get_region(self, ip):
+        ip2region_path = self.ip2region
+        ip2region_path = ip2region_path + ip
+        ret = self.http_handle.get(ip2region_path)
+        if ret[1] != 'SUCC':
+            return ''
+        kv = json.loads(ret[0])
+
+        if 'retData' in kv:
+            if 'city' in kv['retData']:
+                ## discard the last chinese character
+                return kv['retData']['city'][:-1]
+    
+        return ''
+
+    def get_aqi(self):
+        region2aqi_path = self.region2aqi
+        region2aqi_path = region2aqi_path + self.region
+
+        ret = self.http_handle.get(region2aqi_path)
+
+        if ret[1] != 'SUCC':
+            return ''
+
+        kv = json.loads(ret[0])
+
+        if 'retData' in kv:
+            if 'aqi' in kv['retData']:
+                self.aqi = kv['retData']['aqi']
+            if 'level' in kv['retData']:
+                self.level = kv['retData']['level']
+
+            return str(self.aqi) + str(self.level)
+
+        else:
+            return ''
+
+    def get_region_code(self):
+        region2code_path = self.region2code
+        region2code_path = region2code_path  + self.region
+
+        ret = self.http_handle.get(region2code_path)
+
+        if ret[1] != 'SUCC':
+            return ''
+
+        kv = json.loads(ret[0])
+
+        if 'retData' in kv:
+            if 'cityCode' in kv['retData']:
+                return kv['retData']['cityCode']
+
+        return ''
+
+    def get_weather(self):
+        city2weather_path = self.code2weather
+        city2weather_path = city2weather_path + self.regioncode
+
+        ret = self.http_handle.get(city2weather_path)
+        if ret[1] != 'SUCC':
+            return ''
+
+        kv = json.loads(ret[0])
+
+        if 'retData' in kv:
+            if 'weather' in kv['retData']:
+                self.weather = kv['retData']['weather']
+            if 'l_tmp' in kv['retData'] and 'h_tmp' in kv['retData']:
+                self.centigrade = '%s - %s' %(kv['retData']['l_tmp'], kv['retData']['h_tmp'])
+            if 'WD' in kv['retData'] and 'WS' in kv['retData']:
+                self.wind = '%s %s' %(kv['retData']['WD'], kv['retData']['WS'])
+
+            return str(self.weather) + str(self.centigrade) + str(self.wind)
+
+        return ''
+
+    def get_env(self):
+        region = self.get_region(self.ip)
+        if region != '':
+            self.region = region
+        else:
+            return 'FAIL TO GET REGION'
+
+        regioncode = self.get_region_code()
+        if regioncode != '':
+            self.regioncode = regioncode
+        else:
+            return 'FAIL TO GET CITYCODE'
+        
+        weather = self.get_weather()
+        if weather == '':
+            return 'FAIL TO GET WEATHER'
+
+        aqi = self.get_aqi()
+
+        if aqi == '':
+            return 'FAIL TO GET AQI'
+
+        return 'OK'
         
     def GET(self):
         #flash("success", """Welcome! Application code lives in app.py,
         #models in model.py, tests in test.py, and seed data in seed.py.""")
-        return render.index("北京", "晴空万里", "良")
+        self.ip = web.ctx['ip']
+        ret = self.get_env()
+
+        region = '北京'
+        weather = '晴'
+        aqi = '良'
+        if 'OK' != ret:
+            config.logger.warn('fail to init environment [%s][%s]', self.ip, ret)
+        else:
+            region = self.region
+            weather = self.weather + ' ' + self.centigrade
+            aqi = str(self.aqi) + ' ' + self.level + '   ' + self.wind 
+            
+        return render.index(region, weather, aqi)
 
 
 # Set a custom internal error message
---
+++ http_driver.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+import httplib
+import sys
+import os
+import config
+import sys
+
+class HttpDriver():
+
+    def __init__(self, host, logger):
+        self.http_handle = httplib.HTTPConnection(host=host, port=80, timeout=10)
+        self.retry_times = 3
+        self.logger = logger
+
+    def get(self, path):
+        try:
+            for i in xrange(self.retry_times):
+                print path
+                self.http_handle.connect()
+
+                print path
+                self.http_handle.request('GET', path)
+                res = self.http_handle.getresponse()
+
+                if 200 != res.status:
+                    self.http_handle.close()
+                    continue
+
+                body = res.read()
+                self.http_handle.close()
+
+                return body, "SUCC"
+        except Exception, why:
+            self.http_handle.close()
+            return None, "exception[%s]" %(why)
+
+if __name__ == '__main__':
+    hd = HttpDriver('apistore.baidu.com', config.logger)
+    print hd.get('/microservice/iplookup?ip=211.100.31.74')
